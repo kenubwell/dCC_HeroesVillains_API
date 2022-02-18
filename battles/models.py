770bd1b05f1166e507b92f56cce7e59aff9df343
unknown <kenblackuoo@gmail.com>
2022-02-18 18:19:40 -0500

    Created battle model + updated admin file to permit admin interface

--- battles/models.py
+++ battles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from super_types.models import SuperTypes
+from supers.models import Super
 
-# Create your models here.
+class Battle(models.Model):
+    super_one = models.ForeignKey(Super, on_delete=models.CASCADE, related_name='super_one')
+    super_two = models.ForeignKey(Super, on_delete=models.CASCADE, related_name='super_two')
+    battle_date = models.DateTimeField()

f0ffb007238eb602e26b587cabb26378117548a9
unknown <kenblackuoo@gmail.com>
2022-02-18 11:10:28 -0500

    Added comments to the supers app views file + Updated README file to include a brief description of this project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dCC_HeroesVillains_API
+This was a backend project from devCodeCamp's bootcamp designed to evaluate/expand our knowledge of Django REST Framework, to date. 
+The primary task was to build a Django REST API based on the provided Heroes and Villains user stories that met the requirements of the provided Postman collection requests. This project entailed creating multiple apps and using foreign key model relationships. 
+
+Technologies: Python, Django REST Framework, Postman, & MySQL
+
+Resources: Intermediate Django REST Framework (PPT), HeroesVillains Framework (dCC internal document)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,19 @@
+#(5 points) As a developer, I want to create a GET by id endpoint that does the following things:
+#Accepts a value from the request’s URL (the super id) | returns a 200 status code | Responds with the super in the database that has the id that was sent through the URL.
+    #line 52 and lines 56-58
+#(5 points) As a developer, I want to create a POST endpoint that does the following things:
+#Accepts a body object from the request in the form of a Super model | Adds the new super to the database | Returns a 201 status code | Responds with the newly created super object
+    #line 26 and lines 46-50
+#(5 points) As a developer, I want to create a PUT endpoint that does the following things:
+#Accepts a value from the request’s URL (super id) | Finds the super in the Super table and updates that super with the properties that were sent | Returns a 200 status code | Responds with the newly updated super object
+    #line 52 and lines 59-63
+#(5 points) As a developer, I want to create a DELETE endpoint that does the following things:
+# Accepts a value from the request’s URL | Deletes the correct super from the database | Returns a 204 status code (NO CONTENT)
+    #line 52 and lines 64-66
+#(10 points) As a developer, I want to create a GET endpoint the responds with a 200 success status code and all of the supers within the supers table.
+#View function should be implemented in a way to accept a “type” parameter (e.g. http://127.0.0.1:8000/api/supers?type=hero) for "hero" and "villain" | If no type query parameter is sent, return a custom object response with a “heroes” and a "villains" 
+    #line 26 and lines 29-45
+
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -26,7 +42,7 @@ def supers_list(request):
                 custom_response_dictionary[supertype.type] = {
                     "supers": super_serializer.data
                 }        
-            return Response(custom_response_dictionary)
+            return Response(custom_response_dictionary, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializers = SuperSerializer(data=request.data)
         serializers.is_valid(raise_exception=True)  
